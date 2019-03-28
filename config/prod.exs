a634275ab56cd75d3b483950ca7956cb92f71c10
devstar <dev_star11@outlook.com>
2019-03-28 11:04:09 +1000

    bug fix

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :taxlock, TaxlockWeb.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello, Hello.Repo,
+config :taxlock, Taxlock.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
